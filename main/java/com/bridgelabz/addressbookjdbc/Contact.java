0804fb6fc541cada3e8b8a29214307d5c37c0e64
premchandAllu <premchandallu1999@gmail.com>
2020-11-06 16:04:08 +0530

    Added: Multiple Contacts to JSON Server

--- main/java/com/bridgelabz/addressbookjdbc/Contact.java
+++ main/java/com/bridgelabz/addressbookjdbc/Contact.java
@@ -14,7 +14,7 @@ public class Contact {
 	public String email;
 	public String addressBookName;
 	public String addressBookType;
-	LocalDate startDate;
+	public LocalDate startDate;
 
 	public Contact(String firstName, String lastName, String address, String city, String state, int zip,
 			int phoneNumber, String email, String addressBookName, String addressBookType) {
@@ -29,16 +29,7 @@ public class Contact {
 		this.addressBookName = addressBookName;
 		this.addressBookType = addressBookType;
 	}
-	
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		Contact that = (Contact) o;
-		return firstName.equals(that.firstName) && address.equals(that.address);
-	}
+
 	public Contact(String firstName, String lastName, String address, String city, String state, int zip,
 			int phoneNumber, String email, String addressBookName, LocalDate startDate) {
 		this.firstName = firstName;
@@ -50,19 +41,22 @@ public class Contact {
 		this.phoneNumber = phoneNumber;
 		this.email = email;
 		this.addressBookName = addressBookName;
-		this.setStartDate(startDate);
-	}
-
-	public LocalDate getStartDate() {
-		return startDate;
-	}
-
-	public void setStartDate(LocalDate startDate) {
 		this.startDate = startDate;
 	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(firstName, lastName, address, city, state, zip, phoneNumber, email, addressBookName,
 				startDate);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Contact that = (Contact) o;
+		return firstName.equals(that.firstName) && address.equals(that.address);
 	}
 }
--- test/java/com/bridgelabz/addressbookjdbc/AddressBookTest.java
+++ test/java/com/bridgelabz/addressbookjdbc/AddressBookTest.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.addressbookjdbc;
 
+
 import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
@@ -14,23 +15,25 @@ import org.junit.Test;
 import com.bridgelabz.addressbookjdbc.AddressBookService.IOService;
 
 public class AddressBookTest {
-	Logger log = Logger.getLogger(AddressBookTest.class.getName());
+
+	private static Logger log = Logger.getLogger(AddressBookService.class.getName());
 
 	@Test
 	public void contactsWhenRetrievedFromDB_ShouldMatchCount() {
 		AddressBookService addressBookService = new AddressBookService();
 		List<Contact> contactList = addressBookService.readContactData();
-		Assert.assertEquals(3, contactList.size());
+		Assert.assertEquals(4, contactList.size());
 	}
-	
+
 	@Test
 	public void givenNewSalaryForEmployee_WhenUpdatedUsingPreparedStatement_ShouldSyncWithDB() {
 		AddressBookService addressBookService = new AddressBookService();
 		List<Contact> contactList = addressBookService.readContactData();
-		addressBookService.updateContactDetails("Premchand", "Kalpakkam");
-		boolean result = addressBookService.checkContactInSyncWithDB("Premchand");
+		addressBookService.updateContactDetails("Prem", "Chennai");
+		boolean result = addressBookService.checkContactInSyncWithDB("Prem");
 		Assert.assertTrue(result);
 	}
+
 	@Test
 	public void givenDateRange_WhenRetrieved_ShouldMatchEmployeeCount() {
 		AddressBookService addressBookService = new AddressBookService();
@@ -38,32 +41,35 @@ public class AddressBookTest {
 		LocalDate startDate = LocalDate.of(2018, 01, 01);
 		LocalDate endDate = LocalDate.now();
 		List<Contact> contactList = addressBookService.readContactDataForGivenDateRange(startDate, endDate);
-		Assert.assertEquals(2, contactList.size());
+		Assert.assertEquals(6, contactList.size());
 	}
+
 	@Test
 	public void givenContacts_RetrieveNumberOfContacts_ByCityOrState() {
 		AddressBookService addressBookService = new AddressBookService();
 		addressBookService.readContactData();
 		Map<String, Integer> contactByCityOrStateMap = addressBookService.readContactByCityOrState();
-		Assert.assertEquals(true, contactByCityOrStateMap.get("Hyderabad").equals(2));
-		Assert.assertEquals(true, contactByCityOrStateMap.get("TamilNadu").equals(1));
+		Assert.assertEquals(true, contactByCityOrStateMap.get("Hyderabad").equals(3));
+		Assert.assertEquals(true, contactByCityOrStateMap.get("TamilNadu").equals(2));
 	}
+
 	@Test
 	public void givenNewContact_WhenAdded_ShouldSyncWithDB() {
 		AddressBookService addressBookService = new AddressBookService();
 		addressBookService.readContactData();
 		LocalDate date = LocalDate.of(2020, 02, 20);
-		addressBookService.addContactToDatabase("Uma", "Rani", "Whitefield", "Bangalore", "Karnataka", 700012, 99084874,
-				"umarani@gmail.com", "Personal", "Family", date);
+		addressBookService.addContactToDatabase("Arjun", "Reddy", "PTowers", "Bangalore", "Karnataka", 700012, 99084874,
+				"arjunreddy@gmail.com", "Personal", "Family", date);
 		boolean result = addressBookService.checkContactInSyncWithDB("Uma");
 		Assert.assertTrue(result);
 	}
+
 	@Test
 	public void givenContacts_WhenAddedToDB_ShouldMatchEmployeeEntries() {
 		Contact[] arrayOfEmployee = {
-				new Contact("Mohana", "Kavya", "Sathupalli", "Khammam", "Telangana", 507012, 98654331,
-						"mohanakavya@gmail.com", "Casual", LocalDate.now()),
-				new Contact("Mounika", "Anne", "Miyapur", "Hyderabad", "Telangana", 500050, 96763129,
+				new Contact("Kabir", "Singh", "Mumbai", "Mumbai", "Maharastra", 507012, 98654331,
+						"kabirsingh@gmail.com", "Casual", LocalDate.now()),
+				new Contact("Aditya", "varma", "parambakam", "chennai", "Tamilnadu", 500050, 96763129,
 						"mounikaanne@gmail.com", "Personal", LocalDate.now()),
 				new Contact("Sohail", "Syed", "SGNagar", "Kalpakkam", "TamilNadu", 600010, 87655433,
 						"sohailsyed@gmail.com", "Corporate", LocalDate.now()) };
